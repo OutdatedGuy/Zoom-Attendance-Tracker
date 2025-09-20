65a243dbf4dd864d2492a041774e897822427859
OutdatedGuy <everythingoutdated@gmail.com>
2025-09-20 19:35:42 +0530

    feat: updated project Flutter 3.35 template

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "rocks.outdatedguy.zoom_attendance_tracker"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
